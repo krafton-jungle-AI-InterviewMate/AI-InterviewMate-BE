732e1c8e966c88f6c5322fc1bd971b5e3cfaa8d2
Kim gitae <rlarlxo2628@gmail.com>
2023-02-03 21:55:30 +0900

    chore : 프로젝트 파일 init

---
+++ src/main/java/jungle/krafton/AIInterviewMate/AiInterviewMateApplication.java
@@ -0,0 +1,13 @@
+package jungle.krafton.AIInterviewMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInterviewMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jungle/krafton/AIInterviewMate/AiInterviewMateApplicationTests.java
@@ -0,0 +1,13 @@
+package jungle.krafton.AIInterviewMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
