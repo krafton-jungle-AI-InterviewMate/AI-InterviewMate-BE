0dc68bde4f627a9b789953cb8cfca95b18280f01
HyunggiPark <phg9587@naver.com>
2023-02-11 20:06:16 +0900

    feat: [GET] 면접 결과 리스트 API 생성
    #28

--- src/main/java/jungle/krafton/AIInterviewMate/repository/InterviewRoomRepository.java
+++ src/main/java/jungle/krafton/AIInterviewMate/repository/InterviewRoomRepository.java
@@ -1,4 +1,10 @@
 package jungle.krafton.AIInterviewMate.repository;
 
-public interface InterviewRoomRepository {
+import jungle.krafton.AIInterviewMate.domain.InterviewRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InterviewRoomRepository extends JpaRepository<InterviewRoom, Long> {
+    List<InterviewRoom> findAllByMemberIdx(Long memberIdx);
 }
