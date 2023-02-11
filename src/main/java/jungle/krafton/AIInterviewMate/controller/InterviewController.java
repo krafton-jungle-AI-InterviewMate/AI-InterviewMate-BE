db4a5f73c5eea3d1cbc626115189bbd60f951e02
Kim gitae <rlarlxo2628@gmail.com>
2023-02-12 00:00:50 +0900

    fix: conflict 해결

--- src/main/java/jungle/krafton/AIInterviewMate/controller/InterviewController.java
+++ src/main/java/jungle/krafton/AIInterviewMate/controller/InterviewController.java
@@ -1,4 +1,31 @@
 package jungle.krafton.AIInterviewMate.controller;
 
+import jungle.krafton.AIInterviewMate.dto.interview.InterviewRoomInfoDto;
+import jungle.krafton.AIInterviewMate.exception.PrivateResponseBody;
+import jungle.krafton.AIInterviewMate.exception.StatusCode;
+import jungle.krafton.AIInterviewMate.service.InterviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/interview")
 public class InterviewController {
+    private final InterviewService interviewService;
+
+    @Autowired
+    public InterviewController(InterviewService interviewService) {
+        this.interviewService = interviewService;
+    }
+
+
+    @GetMapping("/rooms/{roomIdx}")
+    public ResponseEntity<PrivateResponseBody> getRoomInfo(@PathVariable("roomIdx") long roomIdx) {
+        InterviewRoomInfoDto interviewRoomInfo = interviewService.getRoomInfo(roomIdx);
+        return new ResponseEntity<>(new PrivateResponseBody(StatusCode.OK, interviewRoomInfo), HttpStatus.OK);
+    }
 }
--- src/main/java/jungle/krafton/AIInterviewMate/repository/QuestionRepository.java
+++ src/main/java/jungle/krafton/AIInterviewMate/repository/QuestionRepository.java
@@ -1,4 +1,10 @@
 package jungle.krafton.AIInterviewMate.repository;
 
-public interface QuestionRepository {
+import jungle.krafton.AIInterviewMate.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByQuestionBoxIdx(Long questionBoxIdx);
 }
