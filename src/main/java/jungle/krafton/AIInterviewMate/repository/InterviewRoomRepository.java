bce6bc8ee475f6f3d2646a54e09b4b7da3135460
Kim gitae <rlarlxo2628@gmail.com>
2023-02-11 22:30:24 +0900

    style: Annotation 삭제

--- src/main/java/jungle/krafton/AIInterviewMate/repository/InterviewRoomRepository.java
+++ src/main/java/jungle/krafton/AIInterviewMate/repository/InterviewRoomRepository.java
@@ -2,11 +2,9 @@ package jungle.krafton.AIInterviewMate.repository;
 
 import jungle.krafton.AIInterviewMate.domain.InterviewRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface InterviewRoomRepository extends JpaRepository<InterviewRoom, Long> {
     List<InterviewRoom> findAllByMemberIdx(Long memberIdx);
 
