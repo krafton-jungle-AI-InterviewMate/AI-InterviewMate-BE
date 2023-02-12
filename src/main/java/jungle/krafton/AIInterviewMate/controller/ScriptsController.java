9c1ee5bd5a78de1ec339dc3aa3dbb9911e1b70e1
choihs0457 <85881597+choihs0457@users.noreply.github.com>
2023-02-12 15:18:48 +0900

    feat: 스크립트 저장 구현

--- src/main/java/jungle/krafton/AIInterviewMate/controller/ScriptsController.java
+++ src/main/java/jungle/krafton/AIInterviewMate/controller/ScriptsController.java
@@ -1,4 +1,25 @@
 package jungle.krafton.AIInterviewMate.controller;
 
+import jungle.krafton.AIInterviewMate.exception.PrivateResponseBody;
+import jungle.krafton.AIInterviewMate.exception.StatusCode;
+import jungle.krafton.AIInterviewMate.service.ScriptsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/scripts")
 public class ScriptsController {
+    private final ScriptsService scriptsService;
+
+    public ScriptsController(ScriptsService scriptsService) {
+        this.scriptsService = scriptsService;
+    }
+
+    @PostMapping(value = "/{roomIdx}")
+    public ResponseEntity<PrivateResponseBody> saveRating(@RequestParam("idx") Long questionIdx, @PathVariable Long roomIdx, @RequestBody String script) {
+        scriptsService.saveScript(questionIdx, roomIdx, script);
+        return new ResponseEntity<>(new PrivateResponseBody(StatusCode.OK, null), HttpStatus.OK);
+    }
+
 }
