5be7919c3a916d40519275a7aa1b678d87aea1db
choihs0457 <85881597+choihs0457@users.noreply.github.com>
2023-02-18 01:05:03 +0900

    feat: 질문 리스트와 기워드 가져오기 구현

--- src/main/java/jungle/krafton/AIInterviewMate/controller/QuestionBoxesController.java
+++ src/main/java/jungle/krafton/AIInterviewMate/controller/QuestionBoxesController.java
@@ -1,4 +1,41 @@
 package jungle.krafton.AIInterviewMate.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jungle.krafton.AIInterviewMate.domain.Question;
+import jungle.krafton.AIInterviewMate.exception.PrivateResponseBody;
+import jungle.krafton.AIInterviewMate.exception.StatusCode;
+import jungle.krafton.AIInterviewMate.service.QuestionBoxesService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "questiionBoxes", description = "질문 관련 ")
+@RestController
+@RequestMapping("/questionBoxes")
 public class QuestionBoxesController {
+
+    private final QuestionBoxesService questionBoxesService;
+
+    public QuestionBoxesController(QuestionBoxesService questionBoxesService) {
+        this.questionBoxesService = questionBoxesService;
+    }
+
+    @Operation(summary = "질문 리스트와 키워드 가져오기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content())
+    })
+    @GetMapping("/{questionBoxIdx}")
+    public ResponseEntity<PrivateResponseBody> questionInfo(@PathVariable("questionBoxIdx") Long questionBoxIdx) {
+        List<Question> questionInfo = questionBoxesService.questionsList(questionBoxIdx);
+        return new ResponseEntity<>(new PrivateResponseBody(StatusCode.OK, questionInfo), HttpStatus.OK);
+    }
 }
--- src/main/java/jungle/krafton/AIInterviewMate/service/QuestionBoxesService.java
+++ src/main/java/jungle/krafton/AIInterviewMate/service/QuestionBoxesService.java
@@ -1,4 +1,23 @@
 package jungle.krafton.AIInterviewMate.service;
 
+import jungle.krafton.AIInterviewMate.domain.Question;
+import jungle.krafton.AIInterviewMate.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionBoxesService {
+
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionBoxesService(QuestionRepository questionBoxRepository) {
+        this.questionRepository = questionBoxRepository;
+    }
+
+    public List<Question> questionsList(Long questionBoxIdx) {              //TODO : JWT토근이 완성되면 넘에 값 예외처리
+        return questionRepository.findAllByQuestionBoxIdx(questionBoxIdx);
+    }
 }
