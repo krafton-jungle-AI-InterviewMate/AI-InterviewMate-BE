093fc2f2c80417f25ceaaad7c556e88fe3b80f5b
Kim gitae <rlarlxo2628@gmail.com>
2023-02-28 12:59:09 +0900

    style: RatingUserCOmmentDto import error 수정

--- src/main/java/jungle/krafton/AIInterviewMate/dto/rating/RatingUserCommentDto.java
+++ src/main/java/jungle/krafton/AIInterviewMate/dto/rating/RatingUserCommentDto.java
@@ -1,5 +1,6 @@
 package jungle.krafton.AIInterviewMate.dto.rating;
 
+import jungle.krafton.AIInterviewMate.domain.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
