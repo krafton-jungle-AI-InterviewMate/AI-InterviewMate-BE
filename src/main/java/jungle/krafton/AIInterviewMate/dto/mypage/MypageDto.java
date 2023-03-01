c3a4acc4c9e3b22411daa61f45e98b68e18a9719
HyunggiPark <phg9587@naver.com>
2023-03-01 17:33:47 +0900

    chore: Dto field 변경
    - 필요없는 항목 삭제

--- src/main/java/jungle/krafton/AIInterviewMate/dto/mypage/MypageDto.java
+++ src/main/java/jungle/krafton/AIInterviewMate/dto/mypage/MypageDto.java
@@ -1,13 +1,8 @@
 package jungle.krafton.AIInterviewMate.dto.mypage;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
-@AllArgsConstructor
-@Builder
 @Getter
 public class MyPageDto {
     private String nickname;
-    private String email;
 }
